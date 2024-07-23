231f804b50dcf2c4cc34580b19fd355930785c30
Dmitrii Ivanov <dmitriy.site1@gmail.com>
2024-07-23 09:49:07 -0300

    Add transactions to dependencies

---
+++ src/api/dependencies.py
@@ -0,0 +1,7 @@
+from typing import Annotated
+
+from fastapi import Depends
+
+from src.utils.transaction import BaseManager, TransactionManager
+
+TransactionDep = Annotated[BaseManager, Depends(TransactionManager)]
